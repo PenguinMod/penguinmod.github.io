4162ddab9257b375394ec6872fd2c7edba06d0f3
IcedLuau <141542849+SpaceySlime@users.noreply.github.com>
2025-06-15 15:45:48 +1000

    Update extension-library.jsx

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -45,6 +45,7 @@ const TRUSTED_LOADEXT_ORIGINS = [
     'https://sharkpools-extensions.vercel.app',
     'https://raw.githubusercontent.com/SharkPool-SP/SharkPools-Extensions/main', // Some people cant connect to vercel
     'https://pen-group.github.io',
+    'https://raw.githubusercontent.com/deletelaterMod/mod_SharkPools-Extensions/main'
 ];
 
 class ExtensionLibrary extends React.PureComponent {
