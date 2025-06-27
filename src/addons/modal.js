300a365dd96fb7613531045dc55b85ad6db61d23
mark <94125865+mmmmaaaaarrrrrrkkkkkkkk@users.noreply.github.com>
2025-06-26 19:37:03 -0500

    Revert "kinda rushed commit need to fix some addon modals"
    This reverts commit 1f6c9c47e247f946e1d4cbf7a7280ee3fc61605f.

--- src/addons/modal.js
+++ src/addons/modal.js
@@ -9,23 +9,13 @@ export const createEditorModal = (tab, title, {isOpen = false} = {}) => {
         className: tab.scratchClass('modal_modal-overlay'),
         dir: tab.direction
     });
-    setTimeout(() => {
-        container.classList.add(tab.scratchClass('modal_modal-overlay-visible'));
-    });
     container.style.display = isOpen ? '' : 'none';
     document.body.appendChild(container);
-    const modalOuter = Object.assign(document.createElement('div'), {
-        className: tab.scratchClass('modal_modal-container')
-    });
     const modal = Object.assign(document.createElement('div'), {
-        className: tab.scratchClass('modal_modal-content-addons')
-    });
-    modalOuter.appendChild(modal);
-    setTimeout(() => {
-        modalOuter.classList.add(tab.scratchClass('modal_modal-visible'));
+        className: tab.scratchClass('modal_modal-content')
     });
     modal.addEventListener('click', e => e.stopPropagation());
-    container.appendChild(modalOuter);
+    container.appendChild(modal);
     const header = Object.assign(document.createElement('div'), {
         className: tab.scratchClass('modal_header')
     });
@@ -55,7 +45,7 @@ export const createEditorModal = (tab, title, {isOpen = false} = {}) => {
     });
     modal.appendChild(content);
     return {
-        container: modalOuter,
+        container: modal,
         content,
         backdrop: container,
         closeButton,
