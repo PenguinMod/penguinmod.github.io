db687a1861f7cd011fe69b908e944b72e9d08f5c
mark <94125865+mmmmaaaaarrrrrrkkkkkkkk@users.noreply.github.com>
2025-06-28 19:53:03 -0500

    don't really need a link, I don't have anything on my github

--- src/addons/addons/editor-animations/_manifest_entry.js
+++ src/addons/addons/editor-animations/_manifest_entry.js
@@ -6,8 +6,7 @@ const manifest = {
   "description": "Adds smooth animations to various aspects of the editor!",
   "credits": [
     {
-      "name": "reflow",
-      "link": " https://github.com/mmmmaaaaarrrrrrkkkkkkkk"
+      "name": "reflow"
     },
     {
       "name": "SharkPool"
